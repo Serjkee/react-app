1cb21d178f5d306d937943a87063d21e252df2ca
Siarhei <dreamstar.pwr@gmail.com>
2019-03-17 18:49:42 +0300

    task-2: initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = (props) => (
+  <h1> Hey from {process.env.text} mode</h1>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+ 
+module.exports = env => {
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new webpack.DefinePlugin(envKeys)
+  ]
+  }
+};
