25fcb11f4d3a268e1eeb17af89ad0a9dd8223839
Siarhei <dreamstar.pwr@gmail.com>
2019-03-18 23:47:15 +0300

    task-2: used env variables

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
- 
-module.exports = env => {
+
+module.exports = (env) => {
   const envKeys = Object.keys(env).reduce((prev, next) => {
     prev[`process.env.${next}`] = JSON.stringify(env[next]);
     return prev;
@@ -11,36 +11,37 @@ module.exports = env => {
 
   return {
     entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devtool: env.mode === 'production' ? 'none' : 'cheap-eval-source-map',
+    module: {
+      rules: [{
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          resolve: {
+            extensions: [".js", ".jsx"]
+          },
+          use: {
+            loader: "babel-loader"
           }
-        ]
-      }
+        },
+        {
+          test: /\.html$/,
+          use: [{
+            loader: "html-loader"
+          }]
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      }),
+      new webpack.DefinePlugin(envKeys)
     ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    }),
-    new webpack.DefinePlugin(envKeys)
-  ]
   }
 };
