e830f8e37c2fac537ccf78953c7fd38b16d59a8a
Siarhei Kremis <Siarhei_Kremis@epam.com>
2019-03-19 19:41:27 +0300

    task-2: added uglify plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,28 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = (env) => {
   const envKeys = Object.keys(env).reduce((prev, next) => {
     prev[`process.env.${next}`] = JSON.stringify(env[next]);
     return prev;
   }, {});
+
+  const jsUglifyProduction = new UglifyJsPlugin({
+    test: /\.js(\?.*)?$/i,
+    uglifyOptions: {
+      warnings: false,
+      parse: {},
+      compress: {},
+      mangle: true,
+      output: null,
+      toplevel: false,
+      nameCache: null,
+      ie8: false,
+      keep_fnames: false,
+    },
+  })
 
   return {
     entry: './src/index.js',
@@ -16,6 +32,9 @@ module.exports = (env) => {
       path: path.resolve(__dirname, 'dist')
     },
     devtool: env.mode === 'production' ? 'none' : 'cheap-eval-source-map',
+    optimization: {
+      minimizer: env.mode === 'production' ? [jsUglifyProduction] : [],
+    },
     module: {
       rules: [{
           test: /\.(js|jsx)$/,
