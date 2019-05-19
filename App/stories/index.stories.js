64665c4414e6611c60baf8ffc9cbb2cf26eff754
Siarhei <dreamstar.pwr@gmail.com>
2019-05-20 00:41:01 +0300

    task-8: starting storybook

---
+++ App/stories/index.stories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Button, Welcome } from '@storybook/react/demo';
+import Filter from '../src/components/Search/SearchFilters/Filter/Filter'
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
+
+  storiesOf('Filter', module).add('Filter', () => <Filter 
+    inputId='some id' 
+    inputClass='secondary-filter' 
+    onCustomChange={action('clicked')} 
+    inputName='text' value=''  
+    inputText='Hey' />)
